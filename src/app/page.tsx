1d1153538a6956e5c61a14f1f4968d1bce83af22
AnkitSeal2023 <ankitseal2021@gmail.com>
2025-12-04 08:02:10 +0530

    test: random msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,122 +61,93 @@ export default function Page() {
 	const { scrollYProgress } = useScroll(); // 0 → 1 a user scrolls the whole page
 	const x = useTransform(scrollYProgress, [0, 0.18], [0, -54]);
 	const y = useTransform(scrollYProgress, [0, 0.15], [0, -115]);
-	// const y = useTransform(scrollYProgress, [0, 0.10], [-30, -135]);
-	const scale = useTransform(scrollYProgress, [0, 0.18], [1, 0.3]);
 	return (
-		<div className='flex justify-center items-centerflex-col'>
+		<div className='relative flex w-full items-center justify-center bg-neutral-50 dark:bg-neutral-950'>
+			<div
+				className={cn(
+					"absolute inset-0",
+					"z-0",
+					"[background-size:20px_20px]",
+					"[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+					"dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]",
+				)} />
+			<div className="z-0 pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
 			<Navbar />
-			<div className="h-full max-w-3xl w-full px-5 py-18 ">
-				<div className='flex items-center w-full'>
-				</div>
-				<motion.div
-					style={{
-						x,
-						y,
-						scale,
-						position: "fixed",
-					}}
-					className="rounded-full z-50"
-				>
-					<Image
-						src="/pfp.webp"
-						alt="Ankit Profile Picture"
-						width={150}
-						height={150}
-						className=" rounded-full flex justify-start border p-0.5 border-neutral-300 dark:border-neutral-700"
-					/>
 
-				</motion.div>
-				<div className='flex flex-col md:flex-row pt-40 text-neutral-900 dark:text-neutral-200 text-3xl overflow-visible'>
-					<div className="font-semibold mr-2">
-						Hi, I'm Ankit —
+			<div className="z-10 h-full max-w-3xl w-full px-5 py-18 ">
+				<div className='flex items-center w-full'></div>
+				<div >
+					<div className='flex flex-col md:flex-row pt-40 text-neutral-900 dark:text-neutral-200 text-3xl overflow-visible'>
+						<div className="font-semibold mr-2">
+							Hi, I'm Ankit —
+						</div>
+						<AboutList />
 					</div>
-					<AboutList />
-				</div>
-				<div className='text-neutral-700 dark:text-neutral-400 font-normal py-6'>
-					I do the whole stack—frontend polish, backend brains, and the glue that holds them together on a good day.
-				</div>
-				<div className="flex space-x-3">
-					{/* <div className="cursor-pointer inset-shadow-indigo-500 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-xs font-medium transition-all
-							disabled:pointer-events-none disabled:opacity-50
-							[&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0
-							outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]
-							aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive
-							border bg-background btn-inner-shadow hover:bg-accent hover:text-accent-foreground
-							dark:bg-input/30 dark:border-input dark:hover:bg-input/50 h-9 px-4 py-2 has-[>svg]:px-3
-						    [box-shadow:inset_0_0_2px_0_rgba(99,102,241,0.6)]"
-						onMouseEnter={triggerResumeAnimation}
-						onTouchStartCapture={triggerResumeAnimation}
-					>
-						<motion.div
-							className='rotate-10'
-							animate={resumeControls}
-						>
-							<FileTextIcon />
-						</motion.div>
-						Resumé/CV
-					</div> */}
-					<ResumeCard />
-					<div className="flex justify-center h-9 text-center text-sm">
-						<div
-							onMouseEnter={triggerContactAnimation}
-							onTouchStartCapture={triggerContactAnimation}
-						>
-							<HoverBorderGradient
-								containerClassName="rounded-md"
-								as="button"
-								className="flex items-center space-x-2 h-8 cursor-pointer
+					<div className='text-neutral-700 dark:text-neutral-400 font-normal py-7'>
+						I do the whole stack—frontend polish, backend brains, and the glue that holds them together on a good day.
+					</div>
+					<div className="flex space-x-3">
+						<ResumeCard />
+						<div className="flex justify-center h-9 text-center text-sm">
+							<div
+								onMouseEnter={triggerContactAnimation}
+								onTouchStartCapture={triggerContactAnimation}
+							>
+								<HoverBorderGradient
+									containerClassName="rounded-md"
+									as="button"
+									className="flex items-center space-x-2 h-8 cursor-pointer
                        bg-neutral-900 text-neutral-100
                        dark:bg-neutral-100 dark:text-neutral-900
                        py-4.5 rounded-md"
-							>
-								<motion.div animate={contactControls} className="rotate-10">
-									<Send className="size-4 mr-1 stroke-[1.7]" />
-								</motion.div>
+								>
+									<motion.div animate={contactControls} className="rotate-10">
+										<Send className="size-4 mr-1 stroke-[1.7]" />
+									</motion.div>
 
-								<div className="text-xs font-medium">
-									Get in touch
-								</div>
-							</HoverBorderGradient>
+									<div className="text-xs font-medium">
+										Get in touch
+									</div>
+								</HoverBorderGradient>
+							</div>
 						</div>
-					</div>
 
-				</div>
-				<AnimatedTooltipPreview />
-				<div>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsum quam ea odit eveniet nam! Maxime, aspernatur cumque hic possimus voluptate laudantium ratione perspiciatis iusto, magni eius et earum officiis?
-					Incidunt ullam magni aut dicta adipisci maxime perferendis impedit, similique, aperiam minus error maiores totam cum. Eius aspernatur distinctio culpa, perspiciatis deleniti accusantium assumenda sapiente sit atque fuga nisi alias.
-					Ipsa tenetur officiis necessitatibus voluptatum earum fuga veritatis quibusdam quo delectus reprehenderit, architecto voluptates dolorem maxime voluptas sed quae tempora perspiciatis. Dolorum dolores ad error, saepe ex quam veniam obcaecati.
-					Natus labore facilis tempore odio cupiditate earum voluptas totam dicta exercitationem. Excepturi explicabo possimus consequuntur optio iste deleniti odio temporibus sed expedita, consequatur inventore, mollitia tempore natus quo impedit saepe!
-					Incidunt exercitationem quas odio voluptatum et eius odit esse reprehenderit atque fuga vero nobis distinctio, animi quo officia eaque culpa consequuntur deleniti magni molestias? Minima recusandae laudantium libero numquam consequuntur?
-					Assumenda accusamus at sunt quo, atque voluptatem omnis quia magnam nobis repudiandae expedita iusto natus velit, ducimus porro vel aliquam qui odio sequi, voluptatibus culpa! Recusandae vitae quas eveniet porro!
-					Voluptatem, nihil provident rerum maiores, corrupti perferendis dolorum sed minima itaque necessitatibus ipsam amet harum praesentium consectetur nam quia quaerat placeat saepe deserunt est. Repudiandae tempora omnis ullam minus error!
-					Quae quo dignissimos id delectus corrupti dolor labore eligendi maiores, a odit sunt hic ex veritatis assumenda sapiente beatae itaque et, animi est eaque quia? Suscipit est repellat iure blanditiis?
-					Labore quas, amet nisi tempore repellat sed? Mollitia autem dolorem, at obcaecati nihil, optio saepe iste aspernatur nesciunt omnis ad voluptatum nostrum tenetur, distinctio ex delectus aut vel porro iusto!
-					Commodi deserunt dolore autem alias, beatae animi et reprehenderit soluta qui harum? Expedita hic dignissimos vel eum vitae! Explicabo nostrum magnam qui doloribus minima voluptates, rerum assumenda ratione minus commodi.
-					Ratione fugiat dolorem doloremque facilis cupiditate ipsam et provident voluptate reprehenderit amet quo blanditiis dolore architecto, laudantium molestiae! Quod vel dolorem quos, possimus assumenda commodi placeat consequatur! Maxime, rem id!
-					Voluptatem molestias dolor id. Expedita reprehenderit corrupti doloremque, beatae quo fuga magnam sint cum minima alias perspiciatis. Autem mollitia porro, quod nisi debitis, quia laborum provident similique soluta, esse ullam?
-					Error ratione perspiciatis vel velit culpa magnam magni accusamus sed atque aliquid quod suscipit, tenetur eum harum! Explicabo dolor deserunt nam minima aliquam quam? Ex ut eum minus modi incidunt.
-					Laudantium, corporis, vel amet unde omnis iure nisi, blanditiis expedita sint consequatur odit? Tenetur laboriosam assumenda odit id quam quisquam cumque ipsa aliquid maxime, et corporis, unde sed molestias soluta!
-					Asperiores in quod exercitationem culpa? Aliquam eos velit consectetur illum a sit quaerat iste expedita eaque ipsa numquam, voluptate, vitae accusantium nihil enim hic veritatis nostrum ea, explicabo eum. Maxime.
-					Reprehenderit dolores ducimus in a deleniti perspiciatis officiis maiores rem exercitationem quibusdam omnis illum placeat voluptatibus qui quis porro, unde doloribus nihil facilis. Dolores repellat sequi laboriosam possimus consequuntur voluptates.
-					Autem saepe inventore fugiat facilis voluptas quidem voluptate, nisi magni sed at vel illo vero, nulla velit aperiam libero minima aliquam doloremque eum, facere voluptatibus eveniet nobis. Dolorem, minima earum?
-					Laudantium, eius voluptatem. Nesciunt porro harum reiciendis quasi veritatis? Nisi rerum quasi ratione nostrum illo. Autem quae necessitatibus recusandae, quasi tempora excepturi saepe accusamus a, nulla quos consequatur perferendis non.
-					Vitae neque iure expedita rerum minima. Inventore libero consectetur suscipit perspiciatis, blanditiis nesciunt est officia magnam totam. A doloremque totam eum in, quod optio debitis molestiae nihil sint nemo deserunt.
-					Minus, facere totam eius labore reprehenderit quam esse corporis laborum, molestiae nostrum atque nobis minima, eum quasi voluptate sit. Illo iste autem maiores consectetur, suscipit ducimus quia ipsa non sit.
-					Provident, iure eos aut ut, consectetur laborum dignissimos qui non quia numquam atque nobis odio, repudiandae debitis voluptatem facilis architecto neque consequuntur? Distinctio explicabo, rerum veritatis earum esse est deserunt?
-					Omnis aperiam neque temporibus a delectus quidem officiis, voluptate dolores voluptas vel ducimus quisquam recusandae expedita, enim dolorum eveniet in asperiores fugiat? Voluptates voluptate dicta tenetur aspernatur iusto aliquid porro.
-					Odit aliquid adipisci sunt eveniet quis nisi ullam est obcaecati enim unde maiores vel commodi aut provident quaerat qui reiciendis, ab tempore aperiam quas necessitatibus sed eos praesentium atque. Amet.
-					Corrupti sit corporis unde ratione cupiditate ad nesciunt quasi magnam. Nulla facilis magnam quidem perspiciatis quibusdam porro et quisquam, corrupti in quos, totam expedita. Ipsam molestiae fugit deserunt modi eveniet.
-					Quibusdam minima ut, facilis magnam modi laudantium fugit quos corrupti libero. Quasi perspiciatis, excepturi nam, sequi voluptate ipsam temporibus minima sunt aspernatur cupiditate natus qui expedita dignissimos beatae laudantium rem?
-					Provident eaque ab numquam at ut dignissimos sit eius, nobis dolores ratione deserunt voluptate, doloribus omnis distinctio asperiores voluptates debitis magnam architecto corporis exercitationem, ea a. Atque exercitationem distinctio quia.
-					Recusandae ipsam fugit aut facere enim, iure soluta autem et velit, ea nisi dolores architecto doloremque, iusto dignissimos nemo a. Exercitationem optio nostrum corporis libero quasi tenetur nihil voluptatem necessitatibus!
-					Sequi, illo provident nulla voluptate nostrum accusantium libero error neque omnis ex officia expedita aliquid at amet, et iure natus animi totam explicabo harum vitae veniam eum temporibus. Quis, iusto?
-					Quisquam, minus! Exercitationem quae, tenetur delectus molestias voluptatum repudiandae cumque incidunt natus praesentium maiores ab facere sequi amet eligendi! Beatae dolorum omnis sit nesciunt, accusantium deleniti fugiat vero cumque illo.
-					Delectus temporibus ipsum ratione ipsa ea excepturi sed in ex, asperiores placeat veniam perferendis dolorem nulla tempora tenetur veritatis nemo! Magni suscipit architecto voluptates temporibus nam modi explicabo cum cupiditate!
+					</div>
+					<AnimatedTooltipPreview />
+					<div>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ipsum quam ea odit eveniet nam! Maxime, aspernatur cumque hic possimus voluptate laudantium ratione perspiciatis iusto, magni eius et earum officiis?
+						Incidunt ullam magni aut dicta adipisci maxime perferendis impedit, similique, aperiam minus error maiores totam cum. Eius aspernatur distinctio culpa, perspiciatis deleniti accusantium assumenda sapiente sit atque fuga nisi alias.
+						Ipsa tenetur officiis necessitatibus voluptatum earum fuga veritatis quibusdam quo delectus reprehenderit, architecto voluptates dolorem maxime voluptas sed quae tempora perspiciatis. Dolorum dolores ad error, saepe ex quam veniam obcaecati.
+						Natus labore facilis tempore odio cupiditate earum voluptas totam dicta exercitationem. Excepturi explicabo possimus consequuntur optio iste deleniti odio temporibus sed expedita, consequatur inventore, mollitia tempore natus quo impedit saepe!
+						Incidunt exercitationem quas odio voluptatum et eius odit esse reprehenderit atque fuga vero nobis distinctio, animi quo officia eaque culpa consequuntur deleniti magni molestias? Minima recusandae laudantium libero numquam consequuntur?
+						Assumenda accusamus at sunt quo, atque voluptatem omnis quia magnam nobis repudiandae expedita iusto natus velit, ducimus porro vel aliquam qui odio sequi, voluptatibus culpa! Recusandae vitae quas eveniet porro!
+						Voluptatem, nihil provident rerum maiores, corrupti perferendis dolorum sed minima itaque necessitatibus ipsam amet harum praesentium consectetur nam quia quaerat placeat saepe deserunt est. Repudiandae tempora omnis ullam minus error!
+						Quae quo dignissimos id delectus corrupti dolor labore eligendi maiores, a odit sunt hic ex veritatis assumenda sapiente beatae itaque et, animi est eaque quia? Suscipit est repellat iure blanditiis?
+						Labore quas, amet nisi tempore repellat sed? Mollitia autem dolorem, at obcaecati nihil, optio saepe iste aspernatur nesciunt omnis ad voluptatum nostrum tenetur, distinctio ex delectus aut vel porro iusto!
+						Commodi deserunt dolore autem alias, beatae animi et reprehenderit soluta qui harum? Expedita hic dignissimos vel eum vitae! Explicabo nostrum magnam qui doloribus minima voluptates, rerum assumenda ratione minus commodi.
+						Ratione fugiat dolorem doloremque facilis cupiditate ipsam et provident voluptate reprehenderit amet quo blanditiis dolore architecto, laudantium molestiae! Quod vel dolorem quos, possimus assumenda commodi placeat consequatur! Maxime, rem id!
+						Voluptatem molestias dolor id. Expedita reprehenderit corrupti doloremque, beatae quo fuga magnam sint cum minima alias perspiciatis. Autem mollitia porro, quod nisi debitis, quia laborum provident similique soluta, esse ullam?
+						Error ratione perspiciatis vel velit culpa magnam magni accusamus sed atque aliquid quod suscipit, tenetur eum harum! Explicabo dolor deserunt nam minima aliquam quam? Ex ut eum minus modi incidunt.
+						Laudantium, corporis, vel amet unde omnis iure nisi, blanditiis expedita sint consequatur odit? Tenetur laboriosam assumenda odit id quam quisquam cumque ipsa aliquid maxime, et corporis, unde sed molestias soluta!
+						Asperiores in quod exercitationem culpa? Aliquam eos velit consectetur illum a sit quaerat iste expedita eaque ipsa numquam, voluptate, vitae accusantium nihil enim hic veritatis nostrum ea, explicabo eum. Maxime.
+						Reprehenderit dolores ducimus in a deleniti perspiciatis officiis maiores rem exercitationem quibusdam omnis illum placeat voluptatibus qui quis porro, unde doloribus nihil facilis. Dolores repellat sequi laboriosam possimus consequuntur voluptates.
+						Autem saepe inventore fugiat facilis voluptas quidem voluptate, nisi magni sed at vel illo vero, nulla velit aperiam libero minima aliquam doloremque eum, facere voluptatibus eveniet nobis. Dolorem, minima earum?
+						Laudantium, eius voluptatem. Nesciunt porro harum reiciendis quasi veritatis? Nisi rerum quasi ratione nostrum illo. Autem quae necessitatibus recusandae, quasi tempora excepturi saepe accusamus a, nulla quos consequatur perferendis non.
+						Vitae neque iure expedita rerum minima. Inventore libero consectetur suscipit perspiciatis, blanditiis nesciunt est officia magnam totam. A doloremque totam eum in, quod optio debitis molestiae nihil sint nemo deserunt.
+						Minus, facere totam eius labore reprehenderit quam esse corporis laborum, molestiae nostrum atque nobis minima, eum quasi voluptate sit. Illo iste autem maiores consectetur, suscipit ducimus quia ipsa non sit.
+						Provident, iure eos aut ut, consectetur laborum dignissimos qui non quia numquam atque nobis odio, repudiandae debitis voluptatem facilis architecto neque consequuntur? Distinctio explicabo, rerum veritatis earum esse est deserunt?
+						Omnis aperiam neque temporibus a delectus quidem officiis, voluptate dolores voluptas vel ducimus quisquam recusandae expedita, enim dolorum eveniet in asperiores fugiat? Voluptates voluptate dicta tenetur aspernatur iusto aliquid porro.
+						Odit aliquid adipisci sunt eveniet quis nisi ullam est obcaecati enim unde maiores vel commodi aut provident quaerat qui reiciendis, ab tempore aperiam quas necessitatibus sed eos praesentium atque. Amet.
+						Corrupti sit corporis unde ratione cupiditate ad nesciunt quasi magnam. Nulla facilis magnam quidem perspiciatis quibusdam porro et quisquam, corrupti in quos, totam expedita. Ipsam molestiae fugit deserunt modi eveniet.
+						Quibusdam minima ut, facilis magnam modi laudantium fugit quos corrupti libero. Quasi perspiciatis, excepturi nam, sequi voluptate ipsam temporibus minima sunt aspernatur cupiditate natus qui expedita dignissimos beatae laudantium rem?
+						Provident eaque ab numquam at ut dignissimos sit eius, nobis dolores ratione deserunt voluptate, doloribus omnis distinctio asperiores voluptates debitis magnam architecto corporis exercitationem, ea a. Atque exercitationem distinctio quia.
+						Recusandae ipsam fugit aut facere enim, iure soluta autem et velit, ea nisi dolores architecto doloremque, iusto dignissimos nemo a. Exercitationem optio nostrum corporis libero quasi tenetur nihil voluptatem necessitatibus!
+						Sequi, illo provident nulla voluptate nostrum accusantium libero error neque omnis ex officia expedita aliquid at amet, et iure natus animi totam explicabo harum vitae veniam eum temporibus. Quis, iusto?
+						Quisquam, minus! Exercitationem quae, tenetur delectus molestias voluptatum repudiandae cumque incidunt natus praesentium maiores ab facere sequi amet eligendi! Beatae dolorum omnis sit nesciunt, accusantium deleniti fugiat vero cumque illo.
+						Delectus temporibus ipsum ratione ipsa ea excepturi sed in ex, asperiores placeat veniam perferendis dolorem nulla tempora tenetur veritatis nemo! Magni suscipit architecto voluptates temporibus nam modi explicabo cum cupiditate!
+					</div>
 				</div>
 			</div>
-
 		</div >
 	);
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,12 +6,35 @@ import ThemeToggle from "./theme-toggle";
 
 export default function Navbar() {
 	const { scrollYProgress } = useScroll(); // 0 → 1 a user scrolls the whole page
-	const x = useTransform(scrollYProgress, [0, 0.06, 0.2], [-40, 30, 10]);
+	const x = useTransform(scrollYProgress, [0, 0.04, 0.1], [-40, 30, 10]);
 	const y = useTransform(scrollYProgress, [0, 0.1], [0, 0]);
+
+	const pfpx = useTransform(scrollYProgress, [0, 0.1], [5, -54]);
+	const pfpy = useTransform(scrollYProgress, [0, 0.1], [180, 65]);
+	const scale = useTransform(scrollYProgress, [0, 0.1], [1, 0.3]);
+
 	return (
-		<div className="flex h-16 w-full justify-center fixed top-0 z-10">
+		<div className="flex h-16 w-full justify-center fixed top-0 z-20">
 			<div className="max-w-3xl w-full px-3 h-16 flex justify-between items-center py-4 backdrop-blur-sm ">
 				<div className="text-neutral-900 dark:text-white flex items-end text-sm md:px-1 ">
+					<motion.div
+						style={{
+							x:pfpx,
+							y:pfpy,
+							scale,
+							position: "fixed",
+						}}
+						className="rounded-full z-50"
+					>
+						<Image
+							src="/pfp.webp"
+							alt="Ankit Profile Picture"
+							width={150}
+							height={150}
+							className=" z-50 rounded-full flex justify-start border p-0.5 border-neutral-300 dark:border-neutral-700"
+						/>
+
+					</motion.div>
 					<motion.div
 						style={{
 							x,
