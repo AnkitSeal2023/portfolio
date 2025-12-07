1ba31ff0eced003934ff2832a9a5c8059fdff1dd
AnkitSeal2023 <ankitseal2021@gmail.com>
2025-12-07 13:39:57 +0530

    refactor: updated repo for seo

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: false, // Enforce no trailing slashes (Google prefers consistency)
   images: {
     remotePatterns: [
       {
